e4403887bb65a1440424fc71565dd645da961b0a
Michael Buchli <michael.buchli@wuerth-itensis.com>
2019-03-02 18:04:57 +0100

    CHANGE: gitignore and app js file updated

--- holaMundoQuattro.js
+++ holaMundoQuattro.js
@@ -10,6 +10,7 @@ const rome2rio = require("./routes/rome2rio");
 app.use("/rome2rio", rome2rio);
 
 app.get("*", function(request, response) {
+    response.send('Hola Mundo Quattro!');
 });
 
 app.listen(3001, function () {
